cb6cb9289d5d7cabece23e62688d20fb7c6bb7f4
Macbook Marconato <m.lucalmer@gmail.com>
2019-06-26 17:38:21 +0200

    wrote the code for merging the dataframes with pandas; it requires a lot of RAM and can't be run on my local machine

--- merge.py
+++ merge.py
@@ -1,36 +1,45 @@
 import os
 import pickle
+import progressbar
 
 import pandas as pd
 
-try:
-    merged = pickle.load(open("merged.pickle", "rb"))
-except (OSError, IOError) as e:
-    young_methylation_path = 'MethylationData/Imputed/UD/'
-    files = os.listdir(young_methylation_path)
-    merged = pd.DataFrame(columns=['start_position'])
+def load_methylation_data(folder_path: str) -> pd.DataFrame:
+    pickle_path = folder_path.replace('/', '_') + '.pickle'
+    try:
+        print('loading pickle... ', end='')
+        merged = pickle.load(open(pickle_path, "rb"))
+        print('done')
+    except (OSError, IOError) as e:
+        files = os.listdir(folder_path)
+        merged = pd.DataFrame(columns=['start_position'])
 
-    cell_number = 0
-    for file in files:
-        full_file_path = os.path.join(young_methylation_path, file)
-        df = pd.read_csv(full_file_path, sep='\t', header=0)
-        df.drop(df.columns[list([0, 2])], axis=1, inplace=True)
-        df.columns = ['start_position', 'methylation_state']
-        merged = pd.merge(df, merged, how='outer', on='start_position')
-        merged.rename(columns={'methylation_state': f'file'}, inplace=True)
-    pickle.dump(merged, open("merged.pickle", "wb"))
-    print('done')
+        print('merging methylation files')
+        bar = progressbar.ProgressBar(maxval=len(files),
+                                      widgets=[progressbar.Bar('=', '[', ']'),
+                                               ' ', progressbar.Percentage()])
+        bar.start()
+        i = 1
+        unique_positions = set()
+        for file in files:
+            full_file_path = os.path.join(folder_path, file)
+            df = pd.read_csv(full_file_path, sep='\t', header=0)
+            df.drop(df.columns[list([0, 2])], axis=1, inplace=True)
+            df.columns = ['start_position', 'methylation_state']
+            unique_positions = unique_positions.union(df['start_position'].tolist())
+            # merged = pd.merge(df, merged, how='outer', on='start_position')
+            # merged.rename(columns={'methylation_state': file}, inplace=True)
+            bar.update(i)
+            i += 1
+        bar.finish()
+        print(len(unique_positions))
+        print('pickling... ', end='')
+        pickle.dump(merged, open(pickle_path, "wb"))
+        print('done')
+
+young_methylation_folder_path = 'MethylationData/Imputed/UD/'
+old_methylation_folder_path = 'MethylationData/Imputed/D3/'
+young_methylation_df = load_methylation_data(young_methylation_folder_path)
+old_methylation_df = load_methylation_data(old_methylation_folder_path)
 
 
-def print_full(x):
-    pd.set_option('display.max_rows', len(x))
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 2000)
-    pd.set_option('display.float_format', '{:20,.2f}'.format)
-    pd.set_option('display.max_colwidth', -1)
-    print(x)
-    pd.reset_option('display.max_rows')
-    pd.reset_option('display.max_columns')
-    pd.reset_option('display.width')
-    pd.reset_option('display.float_format')
-    pd.reset_option('display.max_colwidth')
---
+++ utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def print_full(df) -> None:
+    """
+    Prints the full content of the pandas dataframe (which is otherwise truncated)
+
+    :param x: pandas dataframe to print
+    """
+    pd.set_option('didfsplay.max_rows', len(df))
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', -1)
+    print(df)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
